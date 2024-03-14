818d8bb5fff819058a31eb1d67def626dd42856f
vincentanu04 <vincentanu04@gmail.com>
2024-03-15 01:37:55 +0800

    feat: error checking for target controller

--- server/src/controllers/games.controller.ts
+++ server/src/controllers/games.controller.ts
@@ -1,13 +1,17 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Game from '../models/game.model';
 
-const targets = async (req: Request, res: Response) => {
-  const { gameName } = req.params;
-  const [{ targets }] = await Game.find({ name: gameName })
-    .select('targets')
-    .exec();
+const targets = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { gameName } = req.params;
+    const [{ targets }] = await Game.find({ name: gameName })
+      .select('targets')
+      .exec();
 
-  res.json(targets);
+    res.json(targets);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const leaderboard = (req: Request, res: Response) => {
