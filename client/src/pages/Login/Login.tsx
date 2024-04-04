31faca25be67460c2b67bf9dc73d91d93ee464c5
vincentanu04 <vincentanu04@gmail.com>
2024-04-05 01:16:36 +0900

    feat: add placeholder login page

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,5 +1,73 @@
+import {
+  Button,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+} from '@/components/ui';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  username: z.string().min(1, {
+    message: 'Please enter a username.',
+  }),
+  password: z.string().min(1, {
+    message: 'Please enter a password.',
+  }),
+});
+
 const Login = () => {
-  return <div>Login</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='text-white'>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='tulsa1102..' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='text-white'>Password</FormLabel>
+              <FormControl>
+                <Input {...field} type='password' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button variant='secondary' type='submit'>
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Login;
