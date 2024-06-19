fbe31a971423ebd2a6444183a05b89b8abb51bd7
vincentanu04 <vincentanu04@gmail.com>
2024-06-19 11:27:24 +0800

    feat: add signup api in server

--- server/src/controllers/account.controller.ts
+++ server/src/controllers/account.controller.ts
@@ -1,9 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import Account from '../models/account.model';
 
 const signup = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { username, password } = req.body;
     console.log('server:', username, password);
+    const existingAccount = await Account.findOne({ username: username });
+
+    if (existingAccount) {
+      console.log('ACCOUTN ALREADY EXISTS');
+
+      return;
+    }
+
+    console.log('ACCOUNT DOES NOT EXIST');
+
+    await Account.create({ username, password });
   } catch (err) {
     next(err);
   }
