b2d5cab22bf722be0d68bf073d9ebdea143170a2
vincentanu04 <vincentanu04@gmail.com>
2024-06-21 20:33:34 +0800

    feat: implement jwt token creation at signup

--- server/src/controllers/account.controller.ts
+++ server/src/controllers/account.controller.ts
@@ -1,22 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import Account from '../models/account.model';
+import { Types } from 'mongoose';
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id: Types.ObjectId) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' });
+};
 
 const signup = async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw Error('All fields must be filled.');
+  }
 
   try {
     const exists = await Account.findOne({ username: username });
 
     if (exists) {
-      return res.status(400).json({ error: 'Username already in use' });
+      throw new Error('Username already in use.');
     }
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     const user = await Account.create({ username, password: hash });
 
-    res.status(200).json({ username, user });
+    const token = createToken(user._id);
+
+    res.status(200).json({ username, token });
   } catch (err) {
     next(err);
   }
