88cfc53df1d1ac5995b122a714778b8d19c37599
vincentanu04 <vincentanu04@gmail.com>
2024-06-23 19:23:27 +0800

    feat: change api url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://wheres-waldo-mern-api.fly.dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
